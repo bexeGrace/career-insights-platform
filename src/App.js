c1427d8a7e1995a0f7eddf3a02975a22f2d317cb
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-05-15 09:57:38 +0300

    Some Patent modification

--- src/App.js
+++ src/App.js
@@ -152,7 +152,7 @@ export default function App() {
             <Sidenav
               color={sidenavColor}
               brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-              brandName="Material Dashboard 2"
+              brandName="Sitemark Career Insights"
               routes={routes}
               onMouseEnter={handleOnMouseEnter}
               onMouseLeave={handleOnMouseLeave}
@@ -176,7 +176,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Material Dashboard 2"
+            brandName="Sitemark Career Insights"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
